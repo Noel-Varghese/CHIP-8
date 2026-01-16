5b5fe96804b34abcbaea25598a6d6febdb972a8d
Noel <varghesenoel333@gmail.com>
2026-01-16 13:08:59 +0530

    almost finished cpu logic

--- cpu.cpp
+++ cpu.cpp
@@ -187,3 +187,132 @@ void TCpu::add_reg_imm(){
 }//adds value to register
 
 //8XYZ
+void TCpu::decode_8_instruction(){
+    switch (m_current_opcode&0xF)
+    {
+    case 0x0:
+        move_vy_to_vx();
+        break;
+    case 0x1:
+        or_vx_vy();
+        break;
+    case 0x2:
+        and_vx_vy();
+        break;
+    case 0x3:
+        xor_vx_vy();
+        break;
+    case 0x4:
+        add_vx_vy();
+        break;
+    case 0x5:
+        sub_vx_vy();
+        break;
+    case 0x6:
+        shift_right_reg();
+        break;
+    case 0x7:
+        subn_vx_vy();
+        break;
+    case 0xE:
+        shift_left_reg();
+        break;
+    default:
+        m_logger->log("Instruction 8 with code: "+to_string(m_current_opcode&0xF), ELogLevel::ERROR);
+    }
+}//ALU-arithmatic logic unit
+
+//8XY0
+
+void TCpu::move_vy_to_vx(){
+    uint8_t reg_x = (m_current_opcode >> 8)&0x0F;
+    uint8_t reg_y = (m_current_opcode >> 4)&0x0F;
+
+    m_reg[reg_x] = m_reg[reg_y];
+}//moves data between code so tht the registers dont have to recalculate everything
+
+//8XY1
+
+void TCpu::or_vx_vy(){
+    uint8_t reg_x = (m_current_opcode >> 8)&0x0F;
+    uint8_t reg_y = (m_current_opcode >> 4)&0x0F;
+
+    m_reg[reg_x] |= m_reg[reg_y];
+}//performs bitwiseOR in order to combine two registers to set flags, mask values etc
+
+//8XY2
+
+void TCpu::and_vx_vy(){
+    uint8_t reg_x = (m_current_opcode >> 8)&0x0F;
+    uint8_t reg_y = (m_current_opcode >> 4)&0x0F;
+
+    m_reg[reg_x] &= m_reg[reg_y];
+}//performs AND operations , helps in masking bits and shows only the required ones ...Like Abstraction
+
+//8XY3
+void TCpu::xor_vx_vy(){
+    uint8_t reg_x = (m_current_opcode >> 8)&0x0F;
+    uint8_t reg_y = (m_current_opcode >> 4)&0x0F;
+
+    m_reg[reg_x] ^= m_reg[reg_y];
+}//helps toggle and compare values , without branching and extra logic keeps it simple 
+
+//8XY4
+void TCpu::add_vx_vy(){
+    uint8_t reg_x = (m_current_opcode >> 8)&0x0F;
+    uint8_t reg_y = (m_current_opcode >> 4)&0x0F;
+    uint16_t add = m_reg[reg_x]+m_reg[reg_y];
+
+    if(add > 0xFF){
+        m_reg[0xF] = 1;
+    }else{
+        m_reg[0xF] = 0;
+    }   
+     m_reg[reg_x] = add & 0xFF;
+}//allows the program to add two register values while also detecting overflows which is required for the math and game logic
+
+//8XY5
+
+void TCpu::sub_vx_vy(){
+    uint8_t reg_x = (m_current_opcode >> 8)&0x0F;
+    uint8_t reg_y = (m_current_opcode >> 4)&0x0F;
+
+    if(m_reg[reg_x]>m_reg[reg_y]){
+        m_reg[0xF] = 1;
+    }else{
+        m_reg[0xF] = 0;
+    }
+    m_reg[reg_x] -= m_reg[reg_y];
+}//subtracts two registers whilw giving borrow logic, for arithmatic, game logic etc
+
+
+//8XY6
+
+void TCpu::shift_right_reg(){
+    uint8_t reg = (m_current_opcode >> 8) & 0x0F;
+
+    if(m_reg[reg]%2 == 1){
+        m_reg[0xF] = 1;
+    }else{
+        m_reg[0xF] =0;
+    }
+    m_reg[reg] >>= 1;
+}
+//lets the program to divide the value by 2 and to capture the bits tht gets shifted out
+//usefull for arithmatic, movements and bit-level logic
+
+//8XYz
+
+void TCpu::subn_vx_vy(){
+    uint8_t reg_x = (m_current_opcode >> 8) & 0x0F;
+    uint8_t reg_y = (m_current_opcode >> 4) & 0x0F;
+
+    if(m_reg[reg_y] > m_reg[reg_x])
+        m_reg[0xF] = 1;
+    else
+        m_reg[0xF] = 0;
+
+    m_reg[reg_x] = m_reg[reg_y] - m_reg[reg_x];
+}//exist so tht the program can write or subtract in reverse order 
+
+
