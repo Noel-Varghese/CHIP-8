a794703279e041c19ccdae0a7030743cd95be9ea
Noel <varghesenoel333@gmail.com>
2026-01-21 09:45:47 +0530

    Updated displaySDL.cpp

--- displaySDL.cpp
+++ displaySDL.cpp
@@ -8,94 +8,94 @@ TDisplaySDL::TDisplaySDL(){
     m_logger = TLogger::getInstance();
 }
 TDisplaySDL::~TDisplaySDL(){
-    void deinit();
+    
 }
 
-void TDisplaySDL::init(){
-    if(SDL_Init(SDL_INIT_VIDEO)!=0){
-        string errorSdl(SDL_GetError());
-        m_logger->log("SDL initialization error: "+ errorSdl, ELogLevel::ERROR);
-        exit(1);
-    }
-    m_window = SDL_CreateWindow(
-        "CHIP-8", 
-        1280, 720, 0
-    );//used to create a window
-    if(!m_window){
-        //error in window creation
-        string errorWindow(SDL_GetError());
-        m_logger->log("ERROR in creating a Window: "+errorWindow, ELogLevel::ERROR);
-        exit(1);
-    }
-    m_rend = SDL_CreateRenderer(m_window, nullptr);
-    if(!m_rend){
-        string errorRender(SDL_GetError());
-        m_logger->log("ERROR during rendering: "+errorRender, ELogLevel::ERROR);
-        exit(1);
-    }
-    m_texture = SDL_CreateTexture(
-        m_rend, 
-        SDL_PIXELFORMAT_RGBA8888,
-        SDL_TEXTUREACCESS_STREAMING,
-        64,
-        32
-    );
-    if(!m_texture){
-        string errorTexture(SDL_GetError());
-        m_logger->log("ERROR in texture: "+ errorTexture, ELogLevel::ERROR);
+void TDisplaySDL::init()
+{
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) 
+    {
+        std::string errorSdl(SDL_GetError());
+        m_logger->log("SDL Initialization Error: " + errorSdl, ELogLevel::ERROR);
         exit(1);
     }
 
+    // Create a window accordingly 
+    m_window = SDL_CreateWindow("TMVTech chip8",
+                                SDL_WINDOWPOS_UNDEFINED,
+                                SDL_WINDOWPOS_UNDEFINED,
+                                1280, 720, SDL_WINDOW_SHOWN);
+
+    if(!m_window)
+    {
+        std::string errorWindow(SDL_GetError());
+        m_logger->log("Window creation Error: " + errorWindow, ELogLevel::ERROR);
+        exit(1);
+    }
+
+    m_surface = SDL_GetWindowSurface(m_window);
 }
 
-void TDisplaySDL::draw(uint8_t framebuffer[][64], uint16_t height, uint16_t width){
-    //raw texture setup
-    void* pixels = nullptr;//points to raw texture memory
-    int pitch = 0;//number of bytes per row
-    if(SDL_LockTexture(m_texture, nullptr, &pixels, &pitch)!=0){
-        string errortexture(SDL_GetError());
-        m_logger->log("Texture error: "+errortexture, ELogLevel::ERROR);
-        return;
+void setPixel(SDL_Surface *surface, int x, int y, Uint32 color) {
+    if (x >= 0 && x < surface->w && y >= 0 && y < surface->h) {
+        Uint32 *pixels = (Uint32 *)surface->pixels;
+        pixels[(y * surface->w) + x] = color;
     }
-    uint32_t* textPixels = static_cast<uint32_t*>(pixels);//makes pixel useable
-    for(int y =0 ;y<height;y++){
-        for(int x=0;x<width;x++){
-            uint32_t color = framebuffer[y][x]?0xFF00FF00/*green*/:0xFF000000/*Black*/;
-            textPixels[y*(pitch/4)+x] = color;
-        }
-    }   
-    SDL_UnlockTexture(m_texture);
 }
 
-void TDisplaySDL::update(){
-    SDL_RenderClear(m_rend);
+void TDisplaySDL::draw(uint8_t framebuffer[][64], uint16_t width, uint16_t height)
+{
+    uint8_t zoomFactor = 10;
 
-    SDL_FRect dst;//used for scaling and positioning
-    dst.x = 0.0f;//draws from the left edge
-    dst.y = 0.0f;//draws from the top edge
-    dst.w = 64.0f*m_scale;//sets the width
-    dst.h = 32.0f*m_scale;//sets the height
+    if(SDL_MUSTLOCK(m_surface))
+        SDL_LockSurface(m_surface);
 
-    SDL_RenderTexture(m_rend, m_texture, nullptr, &dst);//takes in texture renders it and scales it using dst
-    SDL_RenderPresent(m_rend);//makes the frame visible on screen
+    // Clear the screen
+    Uint32 color = SDL_MapRGB(m_surface->format, 0x00, 0x00, 0x00); // For Black color
 
+    // Fills the entire surface with the black color
+    SDL_FillRect(m_surface, NULL, color);
+
+    Uint32 white =//For white color SDL_MapRGB(m_surface->format, 255, 255, 255); 
+    Uint32 green = SDL_MapRGB(m_surface->format, 0, 255, 0);  //For Green color
+
+    for(auto i=0; i<height; i++)
+    {
+        for(auto j=0; j<width; j++)
+        {
+            if(framebuffer[i][j] == 1)
+            {
+                for (auto x = 0; x < zoomFactor; x++)
+                {
+                    for (auto y = 0; y < zoomFactor; y++)
+                    {
+                        setPixel(m_surface, j*zoomFactor+x, i*zoomFactor+y, green);
+                    }
+                }
+            }
+        }   
+    }
+        
+
+    if(SDL_MUSTLOCK(m_surface))
+        SDL_UnlockSurface(m_surface);
+}
+
+void TDisplaySDL::update()
+{
+    SDL_UpdateWindowSurface(m_window);
     SDL_Event e;
-    while(SDL_PollEvent(&e)){//helps pull all pending events
-        if(e.type == SDL_EVENT_QUIT){
-            m_logger->log("Closing Emulator: ", ELogLevel::INFO);
+    if (SDL_PollEvent(&e) != 0) 
+    {
+        if (e.type == SDL_QUIT) {
+            m_logger->log("CLOSING: ", ELogLevel::ERROR);
         }
     }
 }
 
-void TDisplaySDL::deinit(){
-    if(m_texture){
-        SDL_DestroyTexture(m_texture);
-    }
-    if(m_rend){
-        SDL_DestroyRenderer(m_rend);
-    }
-    if(m_window){
-        SDL_DestroyWindow(m_window);
-    }
+void TDisplaySDL::deinit()
+{
+    SDL_DestroyWindow(m_window);
     SDL_Quit();
 }
